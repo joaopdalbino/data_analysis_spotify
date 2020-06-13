bdfea09cde48291ce6d801c5915564700a35246e
João Albino <joaopdalbino@gmail.com>
2020-06-13 14:13:18 -0300

    new analysis about correlation preparing for multiple linear regression features

--- alg/2 - linear regression.py
+++ alg/2 - linear regression.py
@@ -3,6 +3,8 @@ from scipy import stats
 import os
 from os.path import dirname, abspath
 import pandas as pd
+import numpy as np
+import csv
 
 path = dirname(dirname(abspath(__file__)))
 
@@ -42,6 +44,49 @@ def get_all_indicators_by_column(y, df):
 
 	return data
 
+def get_all_correlations(df):
+
+
+	with open(path + '/results/linear regression/2 - all correlations to be compared.csv', mode='w') as correlation_file:
+
+		correlation_writer = csv.writer(correlation_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+
+		columns = df.columns.tolist()
+
+		columns.insert(0,'')
+
+		correlation_writer.writerow(columns)
+
+		del columns[0]
+
+		l = len(columns)
+
+		while(len(columns) > 0):
+
+			n = columns[0]
+
+			del columns[0]
+
+			x = get_all_features_correlations_from_given_y(n, columns, df)
+
+			while (len(x) < l):
+				x.insert(0,0)
+
+			x.insert(0,n)
+
+			correlation_writer.writerow(x)
+
+def get_all_features_correlations_from_given_y(y, columns, df):
+	
+	x = []
+
+	x.insert(0,1)
+
+	for column in columns:
+		x.append(np.corrcoef(df[y], df[column])[0, 1])
+
+	return x
+
 if __name__ == "__main__":
 
 	print('loading database...')
@@ -51,5 +96,8 @@ if __name__ == "__main__":
 
 	print('creating linear regression indicators based on ' + y + ' ...')
 	get_all_indicators_by_column(y, df).to_csv(path+'/results/linear regression/2 - linear_regression_indicators.csv',index=False)
-	
+
+	print('checking all correlations...')
+	get_all_correlations(df)
+
 	print('... finishing.')
